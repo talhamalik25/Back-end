e7550685f991aed092879ed19910b349b004553d
Talha Malik <talhamalik25>
2026-03-07 23:22:16 +0500

    add

---
+++ Authentication/src/app.js
@@ -0,0 +1,9 @@
+const moongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+module.exports = app;
