2d04dc7abb2c8b0311c90ac60df5297d5f77721a
Talha Malik <talhamalik25>
2026-03-10 21:51:39 +0500

    changes

--- Authentication/src/controllers/auth.controller.js
+++ Authentication/src/controllers/auth.controller.js
@@ -1,7 +1,24 @@
 const userModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
 
 async function register(req, res) {
     const { username, email, password } = req.body;
+
+    const user = await userModel.create({
+        username,
+        email,
+        password
+    });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET,);
+
+        res.status(201).json(
+            {
+                Message: 'User registered successfully',
+                user,
+                token
+            }
+        );
 }
 
 module.exports = {
