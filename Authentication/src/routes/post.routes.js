2f900ec645cab344840656d96c364c2c5d4698df
Talha Malik <talhamalik25>
2026-03-12 01:47:20 +0500

    final

--- Authentication/src/routes/post.routes.js
+++ Authentication/src/routes/post.routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
 
     const token = req.cookies.token;
 
-    if (token) {
+    if (!token) {
         return res.status(401).json({ 
             message: 'Unauthorized' 
         });
@@ -14,11 +14,14 @@ router.post('/create', (req, res) => {
 
    try{
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      console.log(decoded);
+      const user = await userModel.findOne({ 
+        _id: decoded.id 
+    });
+ console.log(user);
    }
    catch(err){
     return res.status(401).json({ 
-        message: 'Unauthorized' 
+        message: 'Token is invalid' 
     });
    }
 
